9825a89cda965094c2b1ffab1759e58047d2cf97
aletheia <bianchi.luca@gmail.com>
2021-02-07 00:24:17 +0100

    :construction: Import function initial WIP. refs #1
    Initial work in progress function

--- src/stack/lib/c19d-services-stack.ts
+++ src/stack/lib/c19d-services-stack.ts
@@ -1,9 +1,14 @@
+import {resolve} from 'path';
 import * as cdk from '@aws-cdk/core';
+import {NodejsFunction} from '@aws-cdk/aws-lambda-nodejs';
 
 export class C19DServicesStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const importFunction = new NodejsFunction(this, 'c19d-import-function', {
+      entry: resolve(__dirname, '../../code/lambda/'),
+      handler: 'index.handler',
+    });
   }
 }
