02edad800c404bb2f29553b0a6b6db6869d3ee16
aletheia <bianchi.luca@gmail.com>
2021-02-07 22:34:42 +0100

    :sparkles: Add application pipeline. refs #2
    Add application pipeline stage containing services stack

--- src/stack/lib/c19d-pipeline-stack.ts
+++ src/stack/lib/c19d-pipeline-stack.ts
@@ -1,31 +1,44 @@
-import {Construct, Stack, StackProps, Stage} from '@aws-cdk/core';
-import {CdkPipeline} from '@aws-cdk/pipelines';
+import {Construct, Stack, StackProps, Stage, SecretValue} from '@aws-cdk/core';
+import {CdkPipeline, SimpleSynthAction} from '@aws-cdk/pipelines';
 import {Artifact} from '@aws-cdk/aws-codepipeline';
+import {GitHubSourceAction} from '@aws-cdk/aws-codepipeline-actions';
 import {C19dServicesStack} from './c19d-services-stack';
 
 class C19dApplication extends Stage {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-    new C19dServicesStack(this, 'c19d-services');
+    new C19dServicesStack(this, 'c19d-services', props);
   }
 }
 
 export class C19dPipelineStack extends Stack {
-  constructor(scope: Construct, id: string, params: StackProps) {
+  constructor(scope: Construct, id: string, props: StackProps) {
     super(scope, id);
     const sourceArtifact = new Artifact();
+    const sourceAction = new GitHubSourceAction({
+      actionName: 'GitHub',
+      output: sourceArtifact,
+      oauthToken: SecretValue.secretsManager('github-token'),
+      owner: 'aletheia',
+      repo: 'covid19-data-services',
+      branch: 'master',
+    });
     const cloudAssemblyArtifact = new Artifact();
 
-    const pipeline = new CdkPipeline(this, 'C19dPipeline', {
+    const synthAction = SimpleSynthAction.standardNpmSynth({
+      sourceArtifact,
       cloudAssemblyArtifact,
+      buildCommand: 'npm run build',
     });
 
-    pipeline.addApplicationStage(
-      new C19dApplication(this, 'C19dApplication', {
-        env: {
-          region: 'eu-south-1',
-        },
-      })
-    );
+    const pipeline = new CdkPipeline(this, 'C19dPipeline', {
+      pipelineName: 'c19d-pipeline',
+      crossAccountKeys: false,
+      cloudAssemblyArtifact,
+      sourceAction,
+      synthAction,
+    });
+
+    pipeline.addApplicationStage(new C19dApplication(this, 'C19dApplication'));
   }
 }
