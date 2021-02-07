ab44ebdcfdd039a20143598cddd9f7e75669fe54
aletheia <bianchi.luca@gmail.com>
2021-02-07 22:42:34 +0100

    :sparkles: Fix linting. refs #2

--- src/code/config/index.ts
+++ src/code/config/index.ts
@@ -1,5 +1,5 @@
 import {config} from 'dotenv';
-import * as merge from 'lodash.merge';
+import merge from 'lodash.merge';
 import {settings} from './settings';
 
 export const connfig = merge({}, settings);
